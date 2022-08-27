f857c9db7a97b61913818df131786860b72f2030
J-unus <kristjan.junus@gmail.com>
2022-08-27 19:47:42 +0300

    Remove unnecessary annotation

--- src/main/java/ee/helmes/sector/SectorApplication.java
+++ src/main/java/ee/helmes/sector/SectorApplication.java
@@ -2,10 +2,8 @@ package ee.helmes.sector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class SectorApplication {
 
 	public static void main(String[] args) {
